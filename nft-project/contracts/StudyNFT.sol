8ea3010ceee22072c7f2fc9950b7a7cbc975549e
temtamk <temtamk@kakao.com>
2023-03-31 10:57:08 +0900

    [UPDATE] Add mintPlanet and metadata for mint

--- nft-project/contracts/StudyNFT.sol
+++ nft-project/contracts/StudyNFT.sol
@@ -2,11 +2,31 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StudyNFT is ERC721 {
-    constructor() ERC721("StudyNFT", "STUDY") {
-        for (uint i = 0; i < 10; i++) {
-            _safeMint(msg.sender, i + 1);
-        }
+contract StudyNFT is ERC721, Ownable {
+  uint256 totalSupply;
+  bool isSaleActive;
+  uint256 MAX_SUPPLY = 100;
+
+  constructor() ERC721("StudyNFT", "STUDY") {}
+
+  function _baseURI() internal view override returns (string memory) {
+      return "ipfs://Qmd3cY6wnjEUFtUCP3R9M9vxyJKqsiZNxoAWhXR3YHiqxM";
+  }
+
+  function setSale(bool active) external onlyOwner {
+      isSaleActive = active;
+  }
+
+  function mintPlanet(uint256 count) external payable {
+    require(isSaleActive, "Sale is not active");
+    require(msg.value >= 0.001 ether * count, "Insufficient ETH");
+    require(count <= 10, "Can mint max 10 planets at a time");
+
+    for (uint i = 0; i < count; i++) {
+        require(totalSupply < MAX_SUPPLY, "Max supply reached");
+        _safeMint(msg.sender, totalSupply++);
     }
+  }
 }
