7d6675cd942808dbba32d9e414b565c2e6184dc2
temtamk <temtamk@kakao.com>
2023-03-31 15:13:42 +0900

    [ADD] deploy script to sepolia

--- nft-project/hardhat.config.ts
+++ nft-project/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const ALCHEMY_API_KEY = "API_KEY";
+const PRIVATE_KEY = "PRIVATE_KEY";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
